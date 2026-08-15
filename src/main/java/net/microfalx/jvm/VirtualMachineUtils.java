ef9d2fe3bdda16703481957d1b173c472a14890c
Adrian Tarau <adrian.tarau@gmail.com>
2026-08-15 14:07:53 -0400

    Fix metrics group name

--- src/main/java/net/microfalx/jvm/VirtualMachineUtils.java
+++ src/main/java/net/microfalx/jvm/VirtualMachineUtils.java
@@ -6,7 +6,7 @@ import oshi.software.common.os.linux.LinuxFileSystem;
 
 public class VirtualMachineUtils {
 
-    public final static Metrics JVM_METRICS = Metrics.ROOT.withGroup("JVM");
+    public final static Metrics JVM_METRICS = Metrics.of("JVM");
     public final static Metrics COLLECTOR_METRICS = JVM_METRICS.withGroup("Collector");
     public final static Metrics METRICS_METRICS = JVM_METRICS.withGroup("Metrics");
 
